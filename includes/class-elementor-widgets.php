0c12802c5f1148d2dc91595d3df4513b43d96179
Astariko-Ambuka <67325387+Astariko-Ambuka@users.noreply.github.com>
2026-08-20 23:21:37 +0300

    Register new appointment widgets in Elementor
    Added new widget files for staff and client appointments.

--- includes/class-elementor-widgets.php
+++ includes/class-elementor-widgets.php
@@ -32,6 +32,7 @@ class NELXJAF_Elementor_Widgets {
             'provider-actions' => NELXJAF_PLUGIN_DIR . 'elementor/widgets/provider-action-buttons.php',
             'client-actions' => NELXJAF_PLUGIN_DIR . 'elementor/widgets/client-action-buttons.php',
             'google-meet' => NELXJAF_PLUGIN_DIR . 'elementor/widgets/google-meet-settings.php',
+            'appointment-listings' => NELXJAF_PLUGIN_DIR . 'elementor/widgets/appointment-listings.php',
         ];
         
         foreach ($widget_files as $file) {
@@ -52,6 +53,16 @@ class NELXJAF_Elementor_Widgets {
         }
         if (class_exists('Elementor_Nelx_Google_Meet_Settings')) {
             $widgets_manager->register(new \Elementor_Nelx_Google_Meet_Settings());
+        }
+        foreach ([
+            'Elementor_Nelx_Staff_Appointments',
+            'Elementor_Nelx_Client_Appointments',
+            'Elementor_Nelx_Staff_Appointments_Grid',
+            'Elementor_Nelx_Client_Appointments_Grid',
+        ] as $widget_class) {
+            if (class_exists($widget_class)) {
+                $widgets_manager->register(new $widget_class());
+            }
         }
     }
     
